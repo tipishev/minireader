fe828c26c930ad9b20eed3fd01022e54e4047d38
Timofey Tipishev <tipishev@yandex-team.ru>
2015-08-24 02:44:14 +0500

    add command line stuff

--- textractor/main.py
+++ textractor/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from sys import argv
+
 from minireader import MiniReader
 
 from extractors import ParentScoreExtractor
@@ -9,21 +11,20 @@ from writers import SimpleWriter, UrlWriter
 def main():
     # URL = 'http://www.gazeta.ru/politics/2015/08/22_a_7711805.shtml'
     # URL = 'http://edition.cnn.com/2015/08/22/europe/europe-macedonia-migrant-crisis/index.html'
-    URL = 'https://slon.ru/posts/55437'
+    # URL = 'https://slon.ru/posts/55437'
+    # URL = 'https://simple.wikipedia.org/wiki/Bishop'
     # URL = 'http://tensor.ru/news/906'
-    # URL = 'http://www.1tv.ru/news/social/290613'  # fail
+    # URL = 'http://www.1tv.ru/news/social/290613'
     # URL = 'http://shender.ru/paper/text/?.file=979'
     # URL = 'http://lenta.ru/news/2015/08/23/boat'
-    WIDTH = 80
+    # URL = 'http://ya.ru'
 
-    mini_reader = MiniReader(
-            extractor=ParentScoreExtractor(),
-            combiner=Combiner(width=WIDTH),
-            # writer=SimpleWriter(),
-            # writer=UrlWriter(URL),
-    )
-    mini_reader.read(URL)
+    if len(argv) != 2:
+        print('USAGE: main.py <url>')
+    else:
+        url = argv[1].rstrip('/')
+        mini_reader = MiniReader()
+        mini_reader.read(url)
 
 if __name__ == '__main__':
-    # TODO rstrip URL slash
     main()
