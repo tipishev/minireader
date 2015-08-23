7a6bb09ffeddfdaeb150314542f6e1ad03c87ea2
Timofey Tipishev <tipishev@yandex-team.ru>
2015-08-24 03:03:11 +0500

    i'm done

--- textractor/config.py
+++ textractor/config.py
@@ -4,3 +4,5 @@
 HTML_PARSER = 'lxml'  # choose from {'lxml', 'html5lib', 'html.parser'}
 LINE_WIDTH = 80
 FILE_ENCODING = 'utf8'
+FILENAME = 'output.txt'
+FANCY_FILENAME = False  # create an URL-like path
--- textractor/main.py
+++ textractor/main.py
@@ -3,27 +3,18 @@
 from sys import argv
 
 from minireader import MiniReader
-
-from extractors import ParentScoreExtractor
-from combiners import Combiner
 from writers import SimpleWriter, UrlWriter
 
-def main():
-    # URL = 'http://www.gazeta.ru/politics/2015/08/22_a_7711805.shtml'
-    # URL = 'http://edition.cnn.com/2015/08/22/europe/europe-macedonia-migrant-crisis/index.html'
-    # URL = 'https://slon.ru/posts/55437'
-    # URL = 'https://simple.wikipedia.org/wiki/Bishop'
-    # URL = 'http://tensor.ru/news/906'
-    # URL = 'http://www.1tv.ru/news/social/290613'
-    # URL = 'http://shender.ru/paper/text/?.file=979'
-    # URL = 'http://lenta.ru/news/2015/08/23/boat'
-    # URL = 'http://ya.ru'
+from config import FANCY_FILENAME, FILENAME
 
+
+def main():
     if len(argv) != 2:
         print('USAGE: main.py <url>')
     else:
         url = argv[1].rstrip('/')
-        mini_reader = MiniReader()
+        writer = UrlWriter(url) if FANCY_FILENAME else SimpleWriter(FILENAME)
+        mini_reader = MiniReader(writer=writer)
         mini_reader.read(url)
 
 if __name__ == '__main__':
